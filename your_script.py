b1b8c438979580010645015944d89dcc1a33bf1f
971023als <971023als@naver.com>
2023-07-12 14:14:52 +0900

    Create your_script.py

---
+++ your_script.py
@@ -0,0 +1,12 @@
+#your_script.py
+
+import sys
+
+def print_python_version():
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
+
+if __name__ == "__main__":
+    print_python_version()
